cbd6159452628df05cc83acf21eb9c010c1f2423
Vitaliy A <vitabr@gmail.com>
2017-10-21 21:24:10 +0300

    Damn, i think i found how to get Trailers :))) not optimized way at all

--- app/src/main/java/com/app4each/tikal/services/GetMoviesService.java
+++ app/src/main/java/com/app4each/tikal/services/GetMoviesService.java
@@ -4,13 +4,15 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 
+import com.app4each.tikal.BuildConfig;
+import com.app4each.tikal.controller.MessageEvent;
 import com.app4each.tikal.model.Movie;
+
+import org.greenrobot.eventbus.EventBus;
 
 import java.util.List;
 
 import info.movito.themoviedbapi.TmdbApi;
-import info.movito.themoviedbapi.TmdbDiscover;
-import info.movito.themoviedbapi.model.Discover;
 import info.movito.themoviedbapi.model.MovieDb;
 import io.realm.Realm;
 
@@ -28,18 +30,22 @@ public class GetMoviesService extends IntentService {
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
 
-        TmdbDiscover api = new TmdbApi("8af268faa6bda7029a469419d1258a14").getDiscover();
-        final List<MovieDb> movies = api.getDiscover(new Discover().sortBy("popularity.desc")).getResults();
+        final TmdbApi api = new TmdbApi(BuildConfig.TMDB_API_KEY);
+        final List<MovieDb> moviesDb = api.getMovies().getPopularMovies("en", 1).getResults();//getDiscover().getDiscover(new Discover().sortBy("popularity.desc")).getResults();
 
         Realm.getDefaultInstance().executeTransaction(new Realm.Transaction() {
             @Override
             public void execute(Realm realm) {
-                for (MovieDb movie: movies) {
-                    Movie movieToAdd = new Movie(movie);
+
+                // Fisrt save initial movies info
+                for (MovieDb movieDb: moviesDb) {
+                    Movie movieToAdd = new Movie(movieDb);
                     realm.copyToRealmOrUpdate(movieToAdd);
                 }
+                EventBus.getDefault().post(new MessageEvent(MessageEvent.EVENT_INITIAL_MOVIE_INFO_RECEIVED));
             }
         });
+
 
     }
 }
